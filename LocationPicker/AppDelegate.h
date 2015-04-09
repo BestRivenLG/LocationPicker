7cb63f8432bc3876871e99e41d3d27a9c3991c92
arvin <rorogestudio@gmail.com>
2015-04-09 11:09:52 +0800

    init

---
+++ LocationPicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationPicker
+//
+//  Created by roroge on 15/4/9.
+//  Copyright (c) 2015年 com.roroge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
